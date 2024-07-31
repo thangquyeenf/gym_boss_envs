0fde044a8c0590a883c605b4907b56bac742bd21
thangquyeenf <thangquyeenf@outlook.com>
2024-07-31 22:52:22 +0700

    update: __init__ GymBoss

--- GymBoss/__init__.py
+++ GymBoss/__init__.py
@@ -2,5 +2,6 @@ from gym.envs.registration import register # type: ignore
 
 register(
     id="GymBoss/BossGame-v0",
-    entry_point="GymBoss.envs:BossEnv"
+    entry_point="GymBoss.envs:BossEnv",
+    max_episode_steps=2000,
 )
